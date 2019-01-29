01e897f247912831c4d81f0dd3b48b5a45e308f8
sambroughton <broughtonjsam@gmail.com>
2019-01-29 14:15:53 +0800

    bay-view initial commit

--- src/views/bay/Bay.js
+++ src/views/bay/Bay.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Bay extends React.Component {
     render() {
-        return <div>BayView</div>
+        return(
+            <div>
+                Initialising the bay
+            </div>
+        );
     }
 }
 
