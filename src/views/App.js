b904d0fef6bf25d10f8684fd991e44580301b27b
sambroughton <broughtonjsam@gmail.com>
2019-01-30 12:26:25 +0800

    Updated routes to not reload html

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar';
 import List from './list/List';
 import Bay from './bay/Bay';
@@ -11,8 +11,10 @@ class App extends React.Component {
             <BrowserRouter>
                 <div className="ui container" style={{margin: '32px 0'}}>
                     <NavBar />
-                    <Route path='/list_view' component={List} />
-                    <Route path='/bay_view' component={Bay} />
+                    <Switch>
+                        <Route path='/list_view' component={List} />
+                        <Route path='/bay_view' component={Bay} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
--- src/views/list/List.js
+++ src/views/list/List.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ListHeader from "../components/ListHeader";
-//import ListTable from "../components/ListTable";
 import TableExampleSortable from "../components/SortableTable"
 
 class List extends React.Component {
